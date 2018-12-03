739bc06c729675fa5544ead12c00f61f24d70e90
joelthp <alisjoel@gmail.com>
2018-12-03 15:48:41 +0100

    gemfile

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.assets.initializers_on_precompile = false
+
 
 module TheFreeNation
   class Application < Rails::Application
