6a1ea7e89b56e3ed5ddc041e434b7b8287614b2e
joelthp <alisjoel@gmail.com>
2018-12-04 23:14:35 +0100

    working_signout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,17 +8,22 @@ class UsersController < ApplicationController
 	def show
 	end
 
+	def edit
+	end
+
 	def destroy
 		@user.destroy
-		redirect_id
+		redirect_to root_path
 	end
 
 
 	private
 	def set_user
-		@user = User.find[params[:id]]
+		@user = User.find(params[:id])
 	end
 
-
+	def user_params
+		params.require(:user).permit(:email, :password)
+	end
 
 end
