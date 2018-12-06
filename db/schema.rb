74cba11c79dd88d02487ac06a7fcf6daf279161e
joelthp <alisjoel@gmail.com>
2018-12-06 17:51:50 +0100

    events up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_05_175457) do
+ActiveRecord::Schema.define(version: 2018_12_06_105827) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,13 +45,11 @@ ActiveRecord::Schema.define(version: 2018_12_05_175457) do
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
-  create_table "follows", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.bigint "event_id"
-    t.index ["event_id"], name: "index_follows_on_event_id"
-    t.index ["user_id"], name: "index_follows_on_user_id"
+  create_table "events_users", id: false, force: :cascade do |t|
+    t.bigint "event_id", null: false
+    t.bigint "user_id", null: false
+    t.index ["event_id", "user_id"], name: "index_events_users_on_event_id_and_user_id"
+    t.index ["user_id", "event_id"], name: "index_events_users_on_user_id_and_event_id"
   end
 
   create_table "languages", force: :cascade do |t|
@@ -85,6 +83,4 @@ ActiveRecord::Schema.define(version: 2018_12_05_175457) do
   add_foreign_key "comments", "events"
   add_foreign_key "comments", "users"
   add_foreign_key "events", "users"
-  add_foreign_key "follows", "events"
-  add_foreign_key "follows", "users"
 end
