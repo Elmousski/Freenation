b22969c9e2288a46e9d2c3f9b3f73f97248662e8
Elmousski <a.moussier@hotmail.fr>
2018-12-09 15:06:52 +0100

    ajout des pages excursions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+Profil.create!(last_name: 'Vanverberghe', first_name: 'Francis', age: '72', title_job: 'Godfather', life_description: "I am just an honnest businessman",	image_user: 'http://static.libertyland.one/images/movies_red/mon-pere-francis-le-belge-8569.jpg?v=30', background_image_user: 'https://camdenhawthorn.com/wp-content/uploads/2018/03/DSC_0657-1580x550.jpg', city:'Marseille', language_1: 'French', 
+
+end
+
