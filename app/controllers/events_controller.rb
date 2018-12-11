d607099504653236e29962d7ca7aeac1287f4528
joelthp <alisjoel@gmail.com>
2018-12-11 18:36:28 +0100

    eventcontroller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,4 @@
-class EventsController < ApplicationController < HomeController 
+class EventsController < ApplicationController 
   before_action :set_event, only: [:show, :edit, :update, :destroy]
 
   def index
