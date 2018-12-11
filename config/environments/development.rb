75affc76539706f54d13c4f5b1b839083aca7f03
joelthp <alisjoel@gmail.com>
2018-12-11 18:22:18 +0100

    mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,12 +61,12 @@ config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # For mail_form gem
-
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors= true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
-    port:                      587,
+    port:                      25,
     domain:                 'gmail.com',
     user_name:           ENV["GMAIL_USERNAME"],
     password:              ENV["GMAIL_PASSWORD"],
