b3fc10ab159894e2b99032b812dc26e2a39c8c15
joelthp <alisjoel@gmail.com>
2018-12-11 17:01:14 +0100

    fusion alex emilie

--- db/schema.rb
+++ db/schema.rb
@@ -92,9 +92,7 @@ ActiveRecord::Schema.define(version: 2018_12_10_095003) do
     t.string "language3"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "user"
     t.bigint "user_id"
-    t.index ["user"], name: "index_profils_on_user"
     t.index ["user_id"], name: "index_profils_on_user_id"
   end
 
