59c88797a08135ef70fc697d6e63ab2aed9a77aa
joelthp <alisjoel@gmail.com>
2018-12-12 14:27:09 +0100

    add some link on index

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-      has_many :languages
       has_many :events
-      has_many :follows
       has_many :comments
-      has_one :profil
 
 end
