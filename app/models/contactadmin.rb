01e338eb752827bdb90a0bf35c4c9033413545c8
JonathanPreminger <jonathan.mete@hotmail.fr>
2018-12-12 12:15:52 +0100

    debut new mailer

---
+++ app/models/contactadmin.rb
@@ -0,0 +1,16 @@
+class Contactadmin  < MailForm::Base
+  attribute :name,      :validate => true
+  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :message
+  attribute :nickname,  :captcha  => true
+
+  # Declare the e-mail headers. It accepts anything the mail method
+  # in ActionMailer accepts.
+  def headers
+    {
+      :subject => "Contact You",
+      :to => "jonathan.mete@hotmail.fr",
+      :from => %("#{name}" <#{email}>)
+    }
+  end
+end
