f2db5a36753cf8e9feac33c9493f0b9e5a9474e4
Elmousski <a.moussier@hotmail.fr>
2018-12-14 17:07:00 +0100

    edit buttton event

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,21 +7,21 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create!(
-	email: "bibim.cup@hotmail.fr",
+	email: "alex@hotmail.fr",
 	password: "123456",
 	password_confirmation: "123456",
-	last_name: "I",
-	first_name: "jack",
-	age: 8,
-	title_job: "dev ruby",
-	life_description: "kiffeur",
-	city: "Paris",
-	language1: "français",
-	language2: "anglais",
-	language3: "chinois",
+	last_name: "Moussier",
+	first_name: "Alexandre",
+	age: "32",
+	title_job: "community manager freelance",
+	life_description: "After being a digital consultant in an agency for three years, i have created my own company 5 years ago as a community manager freelance. Last year, i have been travelling a year in America Latina and i would love to share our first meeting with other digital nomads in Peru. I was there for three months past year, i speak spanish, and i will come back there for a wedding in January 2019. I am going to create our first meeting there, feel free to join.",
+	city: "Montreuil",
+	language1: "french",
+	language2: "english",
+	language3: "spanish",
 )
 
-Event.create!(title_event:"Création d'un réseau Corse-Afrique", #(string)
+Event.create!(title_event:"Création d'un évènement génial", #(string)
 	city:'Libreville', #(string)
 	country:'Gabon', #(string)
 	description_event:"Je cherche des individus louches pour organiser mon réseau entre l'Afrique et la France. Si vous êtes corse, ça m'interesse ", #(content)
@@ -43,7 +43,7 @@ Event.create!(title_event:"Création d'un réseau Corse-Afrique", #(string)
 	price:'300', #(integer)
 	user_id:'1') #(de l’orga uniquement)
 
-Event.create!(title_event:"Création d'un réseau Corse-Afrique", #(string)
+Event.create!(title_event:"Soirée du nouvel an chez Kiki", #(string)
 	city:'Libreville', #(string)
 	country:'Gabon', #(string)
 	description_event:"Je cherche des individus louches pour organiser mon réseau entre l'Afrique et la France. Si vous êtes corse, ça m'interesse ", #(content)
@@ -54,10 +54,10 @@ Event.create!(title_event:"Création d'un réseau Corse-Afrique", #(string)
 	price:'300', #(integer)
 	user_id:'1') #(de l’orga uniquement)
 
-Event.create!(title_event:"Création d'un réseau Corse-Afrique", #(string)
+Event.create!(title_event:"Déménagement", #(string)
 	city:'Libreville', #(string)
 	country:'Gabon', #(string)
-	description_event:"Je cherche des individus louches pour organiser mon réseau entre l'Afrique et la France. Si vous êtes corse, ça m'interesse ", #(content)
+	description_event:"Je cherche des individus louches pour organiser mon déménagement. Si vous êtes corse, ça m'interesse ", #(content)
 	meeting_point:'Paris', #(string)
 	starting_date:'01/01/2019', #(datetime)
 	ending_date:'01/02/2019', #(datetime)
@@ -117,9 +117,10 @@ Event.create!(title_event:"Meeting in Peru - Inca Valley", #(string)
 	city:'Lima', #(string) 
 	country:'Peru', #(string)
 	description_event:"Join a three weeks trip in Peru. We will spend two weeks in a coworking in Lima and then 1 week in cuzco to see the sacred valley and the Macchu Picchu.", #(content)
-	meeting_point:'Lima', #(string)
-	starting_date:'01/02/2019', #(datetime)
-	ending_date:'02/03/2019', #(datetime)
+	meeting_point:'Av. Reducto 1518, Cercado de Lima 15074, Peru',
+	address:'14 avenue dubonnet 92400 courbevoie',
+	starting_date:'28/01/2019', #(datetime)
+	ending_date:'18/02/2019', #(datetime)
 	number_of_places:'10', #(integer)
 	price:'990', #(integer)
 	user_id:'1') #(de l’orga uniquement))
